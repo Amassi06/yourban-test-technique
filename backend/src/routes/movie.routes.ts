1d56864653ce23c1a1908136d2cce504e48a3e06
Massi <aidelmassi@gmail.com>
2026-04-12 00:57:20 +0200

    feat(backend):route CREATE

--- backend/src/routes/movie.routes.ts
+++ backend/src/routes/movie.routes.ts
@@ -3,6 +3,8 @@ import * as movieController from '../controllers/movie.controller.js';
 
 const router = Router();
 
-router.get('/',movieController.getAllMovies);
+router.post('/',movieController.getAllMovies);
+router.get('/',movieController.createMovie);
+
 
 export default router;
