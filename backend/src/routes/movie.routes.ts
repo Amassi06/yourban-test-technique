f039882954872426d58ecd35f9ac8fc794f518ce
Massi <aidelmassi@gmail.com>
2026-04-13 03:59:20 +0200

    feat(frontend):Movie detail page

--- backend/src/routes/movie.routes.ts
+++ backend/src/routes/movie.routes.ts
@@ -4,7 +4,7 @@ import * as movieController from '../controllers/movie.controller.js';
 const router = Router();
 
 router.post('/',movieController.createMovie);
-router.get('/',movieController.getAllMovies);
+router.get('/',movieController.getAlhttp://localhost:3001/api/movies/6lMovies);
 router.patch('/:id', movieController.updateMovie);
 router.delete('/:id',movieController.deleteMovie);
 
